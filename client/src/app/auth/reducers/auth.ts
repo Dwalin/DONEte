aeb634ba5f35207c2192569bce25bc3df6126704
gkatmai <gkatmai@gmail.com>
2016-03-05 17:16:13 +0200

    - auth

--- client/src/app/auth/reducers/auth.ts
+++ client/src/app/auth/reducers/auth.ts
@@ -3,6 +3,7 @@ import {Action, Reducer} from '@ngrx/store';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT_USER = 'LOGOUT_USER';
 
 const initialState = {
     current: null
@@ -12,6 +13,9 @@ export const auth: Reducer<any> = (state = initialState, action: Action) => {
     switch (action.type) {
         case LOGIN_USER:
             return Object.assign({}, {current: action.payload});
+
+        case LOGOUT_USER:
+            return Object.assign({}, {current: null});
 
         default:
             return state;
